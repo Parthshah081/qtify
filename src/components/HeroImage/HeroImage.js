97a03ddf59ecc5697e9870d8f6c5ffe848b49135
Parth Shah <125086541+Parthshah081@users.noreply.github.com>
2023-09-08 17:00:29 +0530

    Second commit

---
+++ src/components/HeroImage/HeroImage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import heroimage from '../../assets/heroimage.png';
+const HeroImage = () => {
+  return (
+    <img src={heroimage} alt='headphone' width={212} height={212}/>
+  )
+}
+
+export default HeroImage
---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './HeroSection.module.css';
+import HeroImage from '../HeroImage/HeroImage';
+
+const HeroSection = () => {
+  return (
+    <div className={styles.container}>
+        <div className={styles.hero}>
+      <h1 className={styles.ad}>100 Thousand Songs, ad-free</h1>
+      <HeroImage/>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
