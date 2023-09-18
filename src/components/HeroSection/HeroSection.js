ee0406ecd7a812bd7bf43b77ab00ce1e0094048b
Parth Shah <125086541+Parthshah081@users.noreply.github.com>
2023-09-18 10:26:46 +0530

    card change

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -7,6 +7,7 @@ const HeroSection = () => {
     <div className={styles.container}>
         <div className={styles.hero}>
       <h1 className={styles.ad}>100 Thousand Songs, ad-free</h1>
+      <h1 className={styles.ad}>Over thousands podcast episodes</h1>
       <HeroImage/>
       </div>
     </div>
