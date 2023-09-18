3aee178d86885edf7cb05efd106b852a3d84d30e
Parth Shah <125086541+Parthshah081@users.noreply.github.com>
2023-09-18 11:02:53 +0530

    sixth message

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
 import './App.css';
+import Card from './components/Card/Card';
 import HeroSection from './components/HeroSection/HeroSection';
 import Navbar from './components/Navbar/Navbar';
+import { fetchTopAlbum } from './api/api';
+import { useEffect, useState } from 'react';
 
 function App() {
+   const [topAlbumsData, setTopAlbumsData] = useState([]);
+  const generateData = async () => {
+      const data = await fetchTopAlbum();
+      setTopAlbumsData(data);
+  }
+
+  useEffect(() => {
+    generateData();
+  }, [])
+
   return (
     <div>
       <Navbar/>
       <HeroSection/>
+      <div className='cards'>
+      {topAlbumsData.map((item) => {
+        return (
+          <Card key={item.id} data={item} type='album'/>
+        )
+      })}
       </div>
+  </div>
   );
 }
 
